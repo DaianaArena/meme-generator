81ae6991198cf9ee548f3406644a352bf0349cf4
DaianaArena <84255114+DaianaArena@users.noreply.github.com>
2022-02-18 17:21:12 -0300

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      APP
+
+       <Navbar />
+
     </div>
-  );
+
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar () {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+            <img src="https://pngimg.com/uploads/trollface/trollface_PNG41.png" alt="logo" />
+            <h1>MEME GENERATOR</h1>
+            </div>
+
+            <p className="autor">Develop by <a href="https://www.linkedin.com/in/arenadaiana/" target="_blank">Daiana Arena</a></p>
+
+        </nav>
+    )
+}
